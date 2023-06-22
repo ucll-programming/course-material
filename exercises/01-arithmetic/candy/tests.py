2b1d1da9e064141eb416603d038bf6650debf7cc
Frederic Vogels <frederic.vogels@ucll.be>
2023-06-22 17:45:18 +0200

    exercises/01-arithmetic/candy

---
+++ exercises/01-arithmetic/candy/tests.py
@@ -0,0 +1,11 @@
+import pytest
+import student
+
+
+@pytest.mark.parametrize('child_count', [1, 2, 5, 10, 14, 19, 200])
+@pytest.mark.parametrize('candy_count', [0, 1, 3, 5, 17, 23, 29, 51, 99, 131, 292, 1000])
+def test_candy_per_child(child_count, candy_count):
+    actual = student.candy_per_child(candy_count=candy_count, child_count=child_count)
+
+    assert candy_count - actual * child_count >= 0
+    assert candy_count - (actual + 1) * child_count < 0
