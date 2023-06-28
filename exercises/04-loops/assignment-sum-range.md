8d3a657fabe126d5e7f5a749641feedc6414cab3
Frederic Vogels <frederic.vogels@ucll.be>
2023-06-28 18:38:32 +0200

    loops/sum-range

---
+++ exercises/04-loops/assignment-sum-range.md
@@ -0,0 +1,11 @@
+# Sum
+
+:::TASK
+Write a function `sum_range(a, b)` that computes the sum
+
+$$
+a + (a + 1) + (a + 2) + \dots + b
+$$
+
+It is guaranteed that `a <= b`.
+:::
--- exercises/04-loops/solutions.py
+++ exercises/04-loops/solutions.py
@@ -5,6 +5,13 @@ def sum_up_to(n):
     return result
 
 
+def sum_range(a, b):
+    result = 0
+    for k in range(a, b+1):
+        result += k
+    return result
+
+
 def factorial(n):
     result = 1
     for k in range(2, n+1):
