649a33f8964a2d269e250a0e718b567cbe9c894c
Frederic Vogels <frederic.vogels@ucll.be>
2023-06-28 18:20:59 +0200

    loops/factorial

--- exercises/04-loops/solutions.py
+++ exercises/04-loops/solutions.py
@@ -3,3 +3,10 @@ def sum_up_to(n):
     for k in range(1, n+1):
         result += k
     return result
+
+
+def factorial(n):
+    result = 1
+    for k in range(2, n+1):
+        result *= k
+    return result
