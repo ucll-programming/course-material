e12c64c8cb0df8d46f30e0465d76e4b169d4222b
Frederic Vogels <frederic.vogels@ucll.be>
2023-07-06 14:39:18 +0200

    wordle uses Enum

--- exercises/projects/wordle/solution.py
+++ exercises/projects/wordle/solution.py
@@ -1,22 +1,30 @@
 from collections import Counter
+from enum import Enum
 
 
-def judge(actual: str, guess: str) -> str:
+class Judgement(Enum):
+    CORRECT = 0
+    MISPLACED = 1
+    WRONG = 2
+
+
+def judge(actual: str, guess: str) -> list[Judgement]:
     if len(actual) != len(guess):
         raise ValueError("actual and guess must be strings of equal length")
+
     length = len(actual)
-    result = ["X"] * length
+    result = [Judgement.WRONG] * length
     counts = Counter(actual)
 
     for i in range(length):
         if actual[i] == guess[i]:
-            result[i] = "C"
+            result[i] = Judgement.CORRECT
             counts[actual[i]] -= 1
 
     for i in range(length):
         letter = guess[i]
-        if counts.get(letter, 0) > 0 and result[i] != "C":
-            result[i] = "M"
+        if counts.get(letter, 0) > 0 and result[i] != Judgement.CORRECT:
+            result[i] = Judgement.MISPLACED
             counts[letter] -= 1
 
-    return "".join(result)
+    return result
--- exercises/projects/wordle/tests.py
+++ exercises/projects/wordle/tests.py
@@ -2,7 +2,7 @@ import pytest
 import student
 
 
-@pytest.mark.parametrize("actual, guess, expected_judgement", [
+@pytest.mark.parametrize("actual, guess, expected_judgement_string", [
     ("A", "A", "C"),
     ("A", "B", "X"),
     ("AA", "AA", "CC"),
@@ -16,6 +16,16 @@ import student
     ("AABCD", "BCDAA", "MMMMM"),
     ("ABABA", "BABAB", "MMMMX"),
 ])
-def test_judge(actual, guess, expected_judgement):
+def test_judge(actual, guess, expected_judgement_string):
+    def parse_judgement(char: str) -> student.Judgement:
+        match char:
+            case "C":
+                return student.Judgement.CORRECT
+            case "M":
+                return student.Judgement.MISPLACED
+            case "X":
+                return student.Judgement.WRONG
+
     actual_judgement = student.judge(actual, guess)
+    expected_judgement = [parse_judgement(judgement) for judgement in expected_judgement_string]
     assert expected_judgement == actual_judgement
