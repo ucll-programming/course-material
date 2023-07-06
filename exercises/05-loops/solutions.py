e4865a727169422e0aa5003ac90574c9a0e8b88e
Frederic Vogels <frederic.vogels@ucll.be>
2023-07-06 11:47:31 +0200

    loops/roman-numerals

--- exercises/05-loops/solutions.py
+++ exercises/05-loops/solutions.py
@@ -53,3 +53,53 @@ def sum_of_input():
         total += number
         number = int(input())
     print(total)
+
+
+def to_roman(number):
+    table = [
+        (1000, 'M'),
+        (900, 'CM'),
+        (500, 'D'),
+        (400, 'CD'),
+        (100, 'C'),
+        (90, 'XC'),
+        (50, 'L'),
+        (40, 'XL'),
+        (10, 'X'),
+        (9, 'IX'),
+        (5, 'V'),
+        (4, 'IV'),
+        (1, 'I'),
+    ]
+
+    result = ""
+    for value, representation in table:
+        while value <= number:
+            number -= value
+            result += representation
+    return result
+
+
+def from_roman(numeral):
+    table = [
+        (1000, 'M'),
+        (900, 'CM'),
+        (500, 'D'),
+        (400, 'CD'),
+        (100, 'C'),
+        (90, 'XC'),
+        (50, 'L'),
+        (40, 'XL'),
+        (10, 'X'),
+        (9, 'IX'),
+        (5, 'V'),
+        (4, 'IV'),
+        (1, 'I'),
+    ]
+
+    result = 0
+    for value, representation in table:
+        while numeral.startswith(representation):
+            result += value
+            numeral = numeral[len(representation):]
+    return result
