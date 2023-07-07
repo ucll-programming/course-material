f86b73ab9ce1ded7d02165c14b6cc8e5bfc200a6
Frederic Vogels <frederic.vogels@ucll.be>
2023-07-07 12:38:59 +0200

    fixed bug in thanos solution

--- exercises/05-loops/solutions.py
+++ exercises/05-loops/solutions.py
@@ -21,8 +21,8 @@ def factorial(n):
 
 def thanos(queue_size, target_size):
     snap_count = 0
-    while queue_size >= target_size:
-        queue_size = queue_size // 2
+    while queue_size > target_size:
+        queue_size //= 2
         snap_count += 1
     return snap_count
 
