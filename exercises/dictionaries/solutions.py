814f6ed2d3b645898fcff31ba9976520626a2909
Frederic Vogels <frederic.vogels@ucll.be>
2023-07-14 10:39:12 +0200

    dictionaries/counts

--- exercises/dictionaries/solutions.py
+++ exercises/dictionaries/solutions.py
@@ -4,3 +4,10 @@ def group_by_first_letter(strings):
         key = string[0].upper()
         result.setdefault(key, []).append(string)
     return result
+
+
+def counts(xs):
+    result = {}
+    for x in xs:
+        result[x] = result.get(x, 0) + 1
+    return result
