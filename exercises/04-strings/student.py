94c3242a07b7ad3f8265e48094d10e9ef8c33659
Frederic Vogels <frederic.vogels@ucll.be>
2023-07-15 17:45:37 +0200

    strings/is-student-id

--- exercises/04-strings/student.py
+++ exercises/04-strings/student.py
@@ -58,3 +58,29 @@ def is_capitalized(string):
     first_char = string[0]
     remaining_chars = string[1:]
     return first_char.upper() == first_char and remaining_chars.lower() == remaining_chars
+
+
+def is_digit(char):
+    return char in '0123456789'
+
+
+def is_student_id(string):
+    if len(string) != 8:
+        return False
+    if string[0].lower() not in 'rs':
+        return False
+    if not is_digit(string[1]):
+        return False
+    if not is_digit(string[2]):
+        return False
+    if not is_digit(string[3]):
+        return False
+    if not is_digit(string[4]):
+        return False
+    if not is_digit(string[5]):
+        return False
+    if not is_digit(string[6]):
+        return False
+    if not is_digit(string[7]):
+        return False
+    return True
