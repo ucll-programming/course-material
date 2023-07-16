d8091445e3960ba9db87987c0706989acacbc4ae
Frederic Vogels <frederic.vogels@ucll.be>
2023-07-16 21:42:46 +0200

    tuples/split-in-two

--- exercises/tuples/explanation-basics.en.md
+++ exercises/tuples/explanation-basics.en.md
@@ -1,5 +1,6 @@
 # Tuples
 
+* Literals (beware of singleton tuple syntax)
 * `[]`, `len`
 * `in` (can't be used for subsequences like strings!)
 * Destructuring
--- exercises/tuples/solutions.py
+++ exercises/tuples/solutions.py
@@ -51,3 +51,8 @@ def add_points(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
     return (x1 + x2, y1 + y2)
+
+
+def split_in_two(xs):
+    index = (len(xs) + 1) // 2
+    return (xs[:index], xs[index:])
