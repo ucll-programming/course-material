06da18387085e7ff12783c3fdb31848a76068307
Frederic Vogels <frederic.vogels@ucll.be>
2023-07-16 17:00:05 +0200

    conditionals/rock-paper-scissors

--- exercises/03-conditionals/solutions.py
+++ exercises/03-conditionals/solutions.py
@@ -20,3 +20,18 @@ def total_cost(amount):
     if amount >= 200:
         amount *= 0.95
     return amount
+
+
+def rock_paper_scissors(player1_choice, player2_choice):
+    rock = 0
+    paper = 1
+    scissors = 2
+    if player1_choice == player2_choice:
+        return 0
+    if player1_choice == rock and player2_choice == scissors:
+        return 1
+    if player1_choice == paper and player2_choice == rock:
+        return 1
+    if player1_choice == scissors and player2_choice == paper:
+        return 1
+    return 2
