7ec0a09a3c5bf3b93c1ee5874f25f739fbd3fd2b
Frederic Vogels <frederic.vogels@ucll.be>
2023-07-16 18:06:24 +0200

    tuples/empty-seats

--- exercises/tuples/solutions.py
+++ exercises/tuples/solutions.py
@@ -23,3 +23,10 @@ def increasing(ns):
         if ns[i-1] > ns[i]:
             return False
     return True
+
+
+def empty_seats(used_seats):
+    if len(used_seats) == 0:
+        return 0
+    total_seats = max(used_seats) - min(used_seats) + 1
+    return total_seats - len(used_seats)
