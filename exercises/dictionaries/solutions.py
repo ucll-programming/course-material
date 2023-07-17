b9da94577741cc0767ce8599563d11ff8efe1d04
Frederic Vogels <frederic.vogels@ucll.be>
2023-07-17 14:25:23 +0200

    dictionaries/election-winner

--- exercises/dictionaries/solutions.py
+++ exercises/dictionaries/solutions.py
@@ -89,3 +89,16 @@ def sell2(stock, model, size):
     if len(available_sizes) == 0:
         del stock[model]
     return True
+
+
+def election_winner(votes):
+    vote_counts = {}
+    winner = None
+    winner_vote_count = 0
+    for vote in votes:
+        updated_count = vote_counts.get(vote, 0) + 1
+        vote_counts[vote] = updated_count
+        if updated_count > winner_vote_count:
+            winner = vote
+            winner_vote_count = updated_count
+    return winner
