ff308762cb58fc4c7c638879308936a99258fb67
Frederic Vogels <frederic.vogels@ucll.be>
2023-07-17 19:05:12 +0200

    tuples/heatwave

--- exercises/tuples/solutions.py
+++ exercises/tuples/solutions.py
@@ -93,3 +93,19 @@ def passing_percentage(grades):
         if grade >= 10:
             passing += 1
     return passing / len(grades) * 100
+
+
+def heatwave(temperatures):
+    above_25_count = 0
+    above_30_count = 0
+    for temperature in temperatures:
+        if temperature < 25:
+            above_25_count = 0
+            above_30_count = 0
+        else:
+            above_25_count += 1
+            if temperature >= 30:
+                above_30_count += 1
+            if above_25_count >= 5 and above_30_count >= 3:
+                return True
+    return False
