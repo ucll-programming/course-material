1207dfeebc27040dc17e525e7efca5bee4d49db2
Frederic Vogels <frederic.vogels@ucll.be>
2023-07-17 14:05:29 +0200

    tuples/election-winner

--- exercises/tuples/solutions.py
+++ exercises/tuples/solutions.py
@@ -56,3 +56,28 @@ def add_points(p1, p2):
 def split_in_two(xs):
     index = (len(xs) + 1) // 2
     return (xs[:index], xs[index:])
+
+
+def count(xs, elt):
+    result = 0
+    for x in xs:
+        if x == elt:
+            result += 1
+    return result
+
+
+def election(votes):
+    votes = sorted(votes)
+    winner = None
+    winner_vote_count = 0
+    i = 0
+    while i < len(votes):
+        j = i
+        while j < len(votes) and votes[i] == votes[j]:
+            j += 1
+        vote_count = j - i + 1
+        if vote_count > winner_vote_count:
+            winner = votes[i]
+            winner_vote_count = vote_count
+        i = j
+    return winner
