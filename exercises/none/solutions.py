169cda0862bf7a9c6e93d5557d71d96b2d19980c
Frederic Vogels <frederic.vogels@ucll.be>
2023-07-18 15:26:35 +0200

    none/product

--- exercises/none/solutions.py
+++ exercises/none/solutions.py
@@ -1,3 +1,13 @@
+def product(a, b, c):
+    if a is None:
+        a = 1
+    if b is None:
+        b = 1
+    if c is None:
+        c = 1
+    return a * b * c
+
+
 def multiple_choice(right_answer, given_answer):
     if right_answer == given_answer:
         return 1
