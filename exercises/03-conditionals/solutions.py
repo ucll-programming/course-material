751339944b0408aa353a1fa1396bd63146c74822
Frederic Vogels <frederic.vogels@ucll.be>
2023-07-19 16:49:25 +0200

    rewrote some solutions in conditionals

--- exercises/03-conditionals/solutions.py
+++ exercises/03-conditionals/solutions.py
@@ -1,17 +1,15 @@
 def my_abs(x):
     if x < 0:
         return -x
-    else:
-        return x
+    return x
 
 
 def sign(x):
     if x < 0:
         return -1
-    elif x == 0:
+    if x == 0:
         return 0
-    else:
-        return 1
+    return 1
 
 
 def total_cost(amount):
