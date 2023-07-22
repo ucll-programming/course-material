520618a08ac3561f59d6403a2ee1cae9abae18ba
Frederic Vogels <frederic.vogels@ucll.be>
2023-07-22 22:14:09 +0200

    fixed file names

--- exercises/lists/test-double-items.py
+++ exercises/lists/test-double-items.py
@@ -13,7 +13,7 @@ import student
         [9, 4, 5, 8, 7, 6, 2, 1],
     ]
 ])
-def test_double_list(ns, expected):
+def test_double_items(ns, expected):
     actual_return_value = student.double_items(ns)
 
     assert actual_return_value is None
