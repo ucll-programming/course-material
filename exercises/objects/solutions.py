1c66fdb1ef51f95be57e151c848ac316d59f2293
Frederic Vogels <frederic.vogels@ucll.be>
2023-07-28 00:06:05 +0200

    objects/averager

--- exercises/objects/solutions.py
+++ exercises/objects/solutions.py
@@ -34,6 +34,23 @@ class Password:
     def verify(self, string):
         return self.__password == string
 
+
+class Averager:
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.__sum = 0
+        self.__count = 0
+
+    def add(self, value):
+        self.__sum += value
+        self.__count += 1
+
+    def average(self):
+        return self.__sum / self.__count
+
+
 class Counter:
     def __init__(self):
         self.__count = 0
