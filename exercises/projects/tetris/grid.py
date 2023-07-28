664fa5692688ef232a156fcaf9425d84e46e013b
Frederic Vogels <frederic.vogels@ucll.be>
2023-07-28 14:45:52 +0200

    tetris Grid

--- exercises/projects/tetris/grid.py
+++ exercises/projects/tetris/grid.py
@@ -1,37 +1,38 @@
-def create(width, height, value):
-    return [
-        [value] * width
-        for _ in range(height)
-    ]
+class Grid:
+    def __init__(self, width, height, value):
+        self.__grid = [
+            [value] * width
+            for _ in range(height)
+        ]
+
+    @property
+    def width(self):
+        return len(self.__grid[0])
+
+    @property
+    def height(self):
+        return len(self.__grid)
 
 
-def width(grid):
-    return len(grid[0])
+    def size(self):
+        return (self.width, self.height)
 
 
-def height(grid):
-    return len(grid)
+    def column(self, index):
+        return [row[index] for row in self.__grid]
 
 
-def size(grid):
-    return (width(grid), height(grid))
+    def transpose(self):
+        return [self.column(index) for index in range(self.width)]
 
 
-def column(grid, index):
-    return [row[index] for row in grid]
+    def rotate_cw(self):
+        return self.transpose(list(reversed(self.__grid)))
 
 
-def transpose(grid):
-    return [column(grid, index) for index in range(width(grid))]
+    def rotate_ccw(self):
+        return self.rotate_cw().rotate_cw().rotate_cw()
 
 
-def rotate_cw(grid):
-    return transpose(list(reversed(grid)))
-
-
-def rotate_ccw(grid):
-    return rotate_cw(rotate_cw(rotate_cw(grid)))
-
-
-def copy(grid):
-    return [row[:] for row in grid]
+    def copy(self):
+        return [row[:] for row in self.__grid]
