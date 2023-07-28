242d53d5278951cc79a253ef8e5ccb4d220f7198
Frederic Vogels <frederic.vogels@ucll.be>
2023-07-28 12:04:13 +0200

    objects/shooting-units

--- exercises/objects/solutions.py
+++ exercises/objects/solutions.py
@@ -64,3 +64,29 @@ class Counter:
 
     def reset(self):
         self.__count = 0
+
+
+
+class Unit:
+    def __init__(self, health, firepower, armor):
+        if health < 0 or firepower < 0 or armor < 0:
+            raise ValueError()
+        self.__health = health
+        self.__firepower = firepower
+        self.__armor = armor
+
+    @property
+    def health(self):
+        return self.__health
+
+    @property
+    def firepower(self):
+        return self.__firepower
+
+    @property
+    def armor(self):
+        return self.__armor
+
+    def shot_by(self, other):
+        health_loss = max(0, other.firepower - self.armor)
+        self.__health = max(0, self.__health - health_loss)
