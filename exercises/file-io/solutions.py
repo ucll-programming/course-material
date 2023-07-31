3784ad9d852ff024a83739af087bd9746d10cbc2
Frederic Vogels <frederic.vogels@ucll.be>
2023-07-31 18:21:08 +0200

    file-io/remove-empty-lines

--- exercises/file-io/solutions.py
+++ exercises/file-io/solutions.py
@@ -4,3 +4,11 @@ def count_lines_in_file(path):
         while file.readline():
             result += 1
     return result
+
+
+def remove_empty_lines(source, destination):
+    with open(source) as in_file:
+        with open(destination, 'w') as out_file:
+            for line in in_file:
+                if line != '\n':
+                    out_file.write(line)
---
+++ exercises/file-io/test-remove-empty-lines.py
@@ -0,0 +1,77 @@
+import pytest
+import student
+
+
+@pytest.mark.timeout(1)
+@pytest.mark.parametrize("contents, expected", [
+    *(
+        ("".join(f'{line}\n' for line in lines), "".join(f'{line}\n' for line in lines if line))
+        for lines in [
+            [
+                ''
+            ],
+            [
+                ' '
+            ],
+            [
+                'a'
+            ],
+            [
+                'a',
+                '',
+            ],
+            [
+                'a',
+                '',
+                '',
+                '',
+            ],
+            [
+                'a',
+                'b',
+            ],
+            [
+                'a',
+                '',
+                'b',
+            ],
+            [
+                'a',
+                '   ',
+                'b',
+            ],
+            [
+                '',
+                'line',
+                '',
+                '',
+                '',
+                'line',
+                '',
+                'line',
+                '',
+                'line',
+                '',
+                'line',
+                '',
+            ]
+        ]
+    )
+])
+@pytest.mark.parametrize('source', [
+    'a.txt',
+    'input.txt',
+])
+@pytest.mark.parametrize('destination', [
+    'b.txt',
+    'output.txt',
+])
+def test_count_lines_in_file(tmp_path, source, destination, contents, expected):
+    source_path = tmp_path / source
+    destination_path = tmp_path / destination
+    source_path.write_text(contents)
+
+    student.remove_empty_lines(str(source_path), str(destination_path))
+
+    assert destination_path.is_file()
+    assert expected == destination_path.read_text()
