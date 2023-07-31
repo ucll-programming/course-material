bcc678d021012febf8024f3f64389a365cfa0c97
Frederic Vogels <frederic.vogels@ucll.be>
2023-07-31 16:53:43 +0200

    updated solution for file-io/count-lines-in-file

--- exercises/file-io/solutions.py
+++ exercises/file-io/solutions.py
@@ -1,3 +1,6 @@
 def count_lines_in_file(path):
+    result = 0
     with open(path) as file:
-        return len(file.readlines())
+        while file.readline():
+            result += 1
+    return result
