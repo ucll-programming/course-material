0acfcf03753446792881d14bfc130ac4d9247bbe
Frederic Vogels <frederic.vogels+github@gmail.com>
2023-10-05 21:07:56 +0200

    booleans/free-ticket

--- exercises/booleans/test-free-ticket.py
+++ exercises/booleans/test-free-ticket.py
@@ -5,13 +5,13 @@ import student
 @pytest.mark.timeout(1)
 @pytest.mark.parametrize('age', [
     *range(0, 12),
-    *range(66, 200),
+    *range(65, 200),
 ])
 def test_free_ticket(age):
     assert student.free_ticket(age)
 
 
 @pytest.mark.timeout(1)
-@pytest.mark.parametrize('age', range(12, 66))
+@pytest.mark.parametrize('age', range(12, 65))
 def test_no_free_ticket(age):
     assert not student.free_ticket(age)
