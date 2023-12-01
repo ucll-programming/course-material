ca54bd0b4227f399b7be9089c610fd04750010f9
Frederic Vogels <frederic.vogels+github@gmail.com>
2023-12-01 20:43:44 +0100

    debugging

--- exercises/debugging/test-is-prime.py
+++ exercises/debugging/test-is-prime.py
@@ -1,5 +1,5 @@
 import pytest
-import isprime
+import student
 
 
 @pytest.mark.timeout(1)
@@ -7,7 +7,7 @@ import isprime
     2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 179424673
 ])
 def test_is_prime(k):
-    assert isprime.is_prime(k), f"is_prime({k}) should return True"
+    assert student.is_prime(k), f"is_prime({k}) should return True"
 
 
 @pytest.mark.timeout(1)
@@ -15,4 +15,4 @@ def test_is_prime(k):
     0, 1, 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20
 ])
 def test_is_not_prime(k):
-    assert not isprime.is_prime(k), f"is_prime({k}) should return False"
+    assert not student.is_prime(k), f"is_prime({k}) should return False"
